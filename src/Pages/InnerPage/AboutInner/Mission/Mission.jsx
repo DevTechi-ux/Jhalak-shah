7f24fea6a1de317d3b0fe4b7ec6d9ef7257333e6
Tarun Dewangan <tarundewangan666@gmail.com>
2025-02-03 15:04:58 +0530

    changes in about

--- src/Pages/InnerPage/AboutInner/Mission/Mission.jsx
+++ src/Pages/InnerPage/AboutInner/Mission/Mission.jsx
@@ -7,19 +7,20 @@ const MissionData = [
     id: 1,
     missionIcon: missionIcon,
     missionTitle: 'Our Mission',
-    missionDesc: 'Professional mision capital without enterp medical users pros value added e-enable creative technology via team.',
+    missionDesc: 'At Rajnayan Clinic, we are dedicated to providing exceptional dental and cosmetic laser treatments tailored to your unique needs. Our mission is to enhance your oral health and aesthetic appearance through advanced technology and compassionate care.',
   },  {
     id: 2,
     missionIcon: missionIcon2,
-    missionTitle: 'Our Planning',
-    missionDesc: 'Professional mision capital without enterp medical users pros value added e-enable creative technology via team.',
+    missionTitle: 'Our Story',
+    missionDesc: 'Founded with a vision to redefine dental and cosmetic care, Rajnayan Clinic has grown into a trusted name in Bhopal. Our journey is marked by a commitment to excellence, continuous learning, and a passion for delivering results that exceed expectations.',
   },  {
     id: 3,
     missionIcon: missionIcon,
-    missionTitle: 'Our Vission',
-    missionDesc: 'Professional mision capital without enterp medical users pros value added e-enable creative technology via team.',
+    missionTitle: 'Our Values',
+    missionDesc: '*Compassionate Care:* We treat every patient with kindness and respect, ensuring a comfortable and welcoming environment. \n *Innovation:* Embracing the latest advancements in dental and cosmetic laser technology to provide effective and efficient treatments. \n *Integrity:* Upholding the highest ethical standards in all our services, fostering trust and transparency.',
   },
 ];
+
 
 const Mission = () => {
   return (
